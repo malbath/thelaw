72384387702ae7d73fcd2f6316f242107ad1140c
Marius ALbath <marius.albath@gmail.com>
2011-06-14 08:47:51 +0200

    update

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 __author__ = 'Tarik'
 import logging
 import random
+import analysis
 
 class SimpleController(object):
     def __init__(self, graph, mr_x, polices, move_cls):
@@ -22,10 +23,18 @@ class TicketOrganizer:
         self.mr_x = mr_x
         self.polices = polices
         self.move_cls = move_cls
+        self.distances = []
 
     def draw(self, round, who):
         choices = self.graph.neighbors(who.get_position())
-        r = random.randint(0, len(choices)-1)
+        if round < 3 :
+			#Die ersten drei Runden chillt man random. :-)
+			r = random.randint(0, len(choices)-1)
+        else :
+			print analysis.get_mr_x_position(self.graph, self.mr_x)
+			#Die Moeglichkeiten werden in die Variable r gespeichert.
+			r = analysis.get_mr_x_position(self.graph, self.mr_x)
+			
         target = choices[r]
         move_type = self.graph[who.get_position()][target][0]['ticket']
         if who.tickets_cab == 0 :
